bbc8f6113f8c745dfb56687f91c6ac00fc9a7ccb
natasharrison <natasharrison@gmail.com>
2021-04-02 10:03:28 -0700

    defined password variables

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var alphaLowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var alphaUpChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numericChar = ["0", "1", "2", "3","4", "5", "6", "7","8", "9"];
+
+var specChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "<", ">", "?", "/", ";", ":"];
+
+// generate password 
+function generatePassword() {
+  
+  
+  var password = window.prompt ("How many characters would you like in your password?");
+  if (password < 8 || password > 128) {
+    window.alert("Your password must contain at least 8 characters and less than 128 characters");
+    } else {
+      confirm ("Would you like your password to contain lowercase characters?");
+      confirm ("Would you like your password to contain Uppercase characters?");
+      confirm ("Would you like your password to contain numeric characters?");
+      confirm ("Would you like your password to contain special characters?");
+    }
+  
+return password; 
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +34,21 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// WHEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
