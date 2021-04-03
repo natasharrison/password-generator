fda463a5c2ad0e4efdbd0295490bcf41a4eac81d
natasharrison <natasharrison@gmail.com>
2021-04-03 12:41:01 -0700

    remove commas final password

--- script.js
+++ script.js
@@ -52,16 +52,17 @@ function generatePassword() {
     console.log(passwordArray)
 
     // for loop 
-    // blank random characters out of password array 
+    // pull blank# random characters out of password array 
 
      for (var i = 0; i < passwordLength; i++) {
        var randomNum = Math.floor(Math.random() * passwordArray.length)
-       finalPassword.push(passwordArray[randomNum])
+       finalPassword.push(passwordArray[randomNum]);
        }
+      console.log(finalPassword);
 
-       console.log(finalPassword);
-
-      //  remove the commas 
+    //  remove the commas      
+    finalPassword = finalPassword.join('');
+   
 return finalPassword; 
 
 };
@@ -77,19 +78,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// WHEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-
-
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
